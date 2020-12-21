87822fb7a48f7e724118820ba9e81145b168b1dc
Jan <jan.bogoryja@gmail.com>
2020-12-21 10:52:01 +0100

    Add a Student Entity
    The student now exists in code. Wowzers

---
+++ src/main/java/dev/hotdeals/sonito/model/Student.java
@@ -0,0 +1,69 @@
+package dev.hotdeals.sonito.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "student")
+public class Student
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Basic
+    @Column(name = "name", length = 160)
+    private String name;
+
+    @Basic
+    @Column(name = "email", length = 255)
+    private String email;
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id &&
+                Objects.equals(name, student.name) &&
+                Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(id, name, email);
+    }
+}
