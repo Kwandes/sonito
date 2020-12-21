57b2e71404f456ed247b643063771cfe84a1577c
Jan <jan.bogoryja@gmail.com>
2020-12-21 10:42:42 +0100

    Add Uml for Student entity
    Yes
    I did

---
+++ src/main/java/dev/hotdeals/sonito/uml/StudentUml.java
@@ -0,0 +1,15 @@
+package dev.hotdeals.sonito.uml;
+
+public class StudentUml
+{
+    public String toString()
+    {
+        return  "    +-----------------------------------+\n" +
+                "    |   student                         |\n" +
+                "    +-----------------------------------+\n" +
+                "    |   id : INTEGER(11)                |\n" +
+                "    |   name: VARCHAR(160)              |\n" +
+                "    |   email: VARCHAR(255)             |\n" +
+                "    +-----------------------------------+";
+    }
+}
