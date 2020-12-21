8bf84b704c56cb2bfdf6eb9a8e76edb6daca1a2e
Jan <jan.bogoryja@gmail.com>
2020-12-21 11:58:28 +0100

    Add UML for supervisor entity

---
+++ src/main/java/dev/hotdeals/sonito/supervisor/SupervisorUml.java
@@ -0,0 +1,15 @@
+package dev.hotdeals.sonito.supervisor;
+
+public class SupervisorUml
+{
+    public String toString()
+    {
+        return  "    +-----------------------------------+\n" +
+                "    |   supervisor                      |\n" +
+                "    +-----------------------------------+\n" +
+                "    |   id : INTEGER(11)                |\n" +
+                "    |   name: VARCHAR(160)              |\n" +
+                "    |   email: VARCHAR(255)             |\n" +
+                "    +-----------------------------------+";
+    }
+}
