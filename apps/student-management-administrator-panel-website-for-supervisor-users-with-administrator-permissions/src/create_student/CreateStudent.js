d678f8336b8bf68903c469e93359e3286f7c0942
Jan <jan.bogoryja@gmail.com>
2020-12-21 18:46:57 +0100

    Add API call for creation of a student
    No input validation as of now

--- apps/student-management-administrator-panel-website-for-supervisor-users-with-administrator-permissions/src/create_student/CreateStudent.js
+++ apps/student-management-administrator-panel-website-for-supervisor-users-with-administrator-permissions/src/create_student/CreateStudent.js
@@ -1,16 +1,70 @@
 import React from 'react';
 import './CreateStudent.css'
+import HttpService from "../service/HttpService";
 
 export default class CreateStudent extends React.Component
 {
+
+    constructor(props)
+    {
+        super(props);
+        this.createUser = this.createUser.bind(this);
+
+        this.state = {
+            name: "",
+            email: "",
+            supervisorId: 0
+        }
+    }
+
+    createUser()
+    {
+        console.log(this.state.name + " " + this.state.email + " " + this.state.supervisorId)
+
+
+        HttpService.post("/student", {
+            name: this.state.name,
+            email: this.state.email,
+            supervisor: {
+                id: this.state.supervisorId
+            }
+        })
+            .then((response) =>
+            {
+                console.log("createStudent Response :");
+                console.log(response.data);
+            })
+            .catch((e) =>
+            {
+                console.log(e);
+            });
+    }
+
+    updateInfo = (type, text) =>
+    {
+        if (type === 'name')
+        {
+            this.setState({name: text});
+        } else if (type === 'email')
+        {
+            this.setState({email: text});
+        } else if (type === 'supervisorId')
+        {
+            this.setState({supervisorId: text});
+        }
+    }
+
     render()
     {
         return (
             <div id={'StudentContainer'}>
                 <h3>Create your very own Student</h3>
-                <input type="text" placeholder={"name"}/>
-                <input type="text" placeholder={"email"}/>
-                <input type="number" placeholder={"supervisor ID"}/>
+                <input type="text" placeholder={"name"} onChange={e => this.updateInfo('name', e.target.value)}/>
+                <input type="text" placeholder={"email"} onChange={e => this.updateInfo('email', e.target.value)}/>
+                <input type="number" placeholder={"supervisor ID"}
+                       onChange={e => this.updateInfo('supervisorId', e.target.value)}/>
+                <input type={'button'} id={'CreateStudentBtn'} value={'Create Student'}
+                       onClick={() => this.createUser()}/>
             </div>
         )
     }
