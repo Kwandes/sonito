c27a3f07925dfa74963bc1316ce50535d3bd55b0
Jan <jan.bogoryja@gmail.com>
2020-12-21 09:45:19 +0100

    initialize the Springboot project
    Generate and initialize a Java Springboot app, with MySQL connection included

---
+++ src/main/java/dev/hotdeals/sonito/SonitoApplication.java
@@ -0,0 +1,13 @@
+package dev.hotdeals.sonito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonitoApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SonitoApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/hotdeals/sonito/SonitoApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.hotdeals.sonito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonitoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
